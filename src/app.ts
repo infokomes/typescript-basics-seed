b2fa09814e4630d3a18a9a2e751d2b7d63336dce
Arkadiusz Chrobak <chrobak.arkadiusz@gmail.com>
2018-03-03 18:58:21 +0100

    arrow-functions

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
-console.log('Hello TypeScript!');
+const pizzas = [{ name: 'Pepperoni', toppings: ['pepperoni'] }];
+
+const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
+
+// const mappedPizzas = pizzas.map(pizza => {
+//   return pizza.name.toUpperCase();
+// });
+
+console.log(mappedPizzas);
+
+function createPizza() {}
+
+const pizza = {
+  name: 'Blazing Inferno',
+  getName: () => pizza.name,
+  getNameFunction: function() {
+    console.log(this.name);
+  },
+  getNameTimeout: function() {
+    setTimeout(() => {
+      console.log(this.name);
+    }, 100);
+  },
+};
+
+console.log(pizza.getName());
