4386abc126ab992d0336a2d0b9d6274af99fdf9c
Arkadiusz Chrobak <chrobak.arkadiusz@gmail.com>
2018-03-03 19:47:02 +0100

    Destructuring Objects

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,27 @@
-const toppings = ['bacons', 'chili'];
+// Destructuring Objects
 
-const newToppings = ['pepperoni'];
+// import { uppercase } from './utils';
 
-const allTopings = [...toppings, ...newToppings];
+const pizza = {
+  name: 'Pepperoni',
+  toppings: ['pepperoni'],
+};
 
-console.log(allTopings);
+function order({ name: pizzaName, toppings: pizzaTopings }) {
+  // console.log(pizzaName, pizzaTopings)
+  return { pizzaName, pizzaTopings };
+}
+
+const { pizzaName } = order(pizza);
+
+const toppings = ['pepperoni', 'bacon', 'chilli'];
+
+const [first, second, third] = toppings;
+
+// console.log(first, second, third);
+
+function logToppings([first, second, third]: any) {
+  console.log(first, second, third);
+}
+
+logToppings(toppings);
