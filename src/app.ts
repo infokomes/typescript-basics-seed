62a84b282ea7963500ead92567552cf157e99c82
Arkadiusz Chrobak <chrobak.arkadiusz@gmail.com>
2018-03-04 18:37:21 +0100

    Type Aliases and Assertions

--- src/app.ts
+++ src/app.ts
@@ -1,124 +1,26 @@
-// // The "Any" Type
+// // Type Aliases
 
-// let coupon: any;
+// type Size = 'small' | 'medium' | 'large';
+// type Callback = (size: Size) => void;
 
-// coupon = 25;
-// coupon = `pizza25`;
-// coupon = false;
+// let pizzaSize: Size = 'small';
 
-// // Implicit vs Explicit Types
-
-// let implicitCoupon = 'pizza25';
-// let explicitCoupon: string;
-
-// explicitCoupon = 'pizza25';
-
-// // Void Type
-
-// let selectedTopping: string = 'pepperoni';
-
-// function selectTopping(topping: string): void {
-//   selectedTopping = topping;
-// }
-
-// selectTopping('bacon');
-
-// console.log(selectedTopping);
-
-// // Never Type
-
-// function orederError(error: string): never {
-//   throw new Error(error);
-//   // never going return value
-// }
-
-// orederError('Something went wrong');
-
-// // Null, Undefined, Strict Null checks
-
-// let coupon: string | null = 'pizza25';
-
-// function removeCoupon(): void {
-//   coupon = null;
-// }
-
-// console.log(coupon);
-
-// removeCoupon();
-
-// console.log(coupon);
-
-// // Union and Literal Types
-
-// let pizzaSize: string = 'small';
-
-// function selectSize(size: 'small' | 'medium' | 'large'): void {
-//   pizzaSize = size;
-// }
-
-// selectSize('large');
-
-// console.log(`Pizza size: ${pizzaSize}`);
-
-// // Function Types
-
-// // let sumOrder: Function
-// let sumOrder: (price: number, quantity: number) => number = (x, y) => x * y;
-
-// const sum = sumOrder(25, 2);
-
-// console.log(`Total sum: ${sum}`);
-
-// // Functions and Optional Arguments
-
-// let sumOrder: (price: number, quantity?: number) => number;
-
-// sumOrder = (x, y) => {
-//   if (y) {
-//     return x * y;
-//   }
-//   return x;
+// const selectSize: Callback = x => {
+//   pizzaSize = x;
 // };
 
-// const sum = sumOrder(25);
-// sumOrder(25, 2);
+// selectSize('medium');
 
-// // Typed Functions and Default Params
+// // Type Assertions
 
-// let sumOrder: (price: number, quantity?: number) => number;
+type Pizza = { name: string; toppings: number };
 
-// sumOrder = (x, y = 1) => x * y;
+const pizza: Pizza = { name: 'Blazing Inferno', toppings: 5 };
 
-// const sum = sumOrder(25, 5);
+const serialized = JSON.stringify(pizza);
 
-// console.log(`Total sum: ${sum}`);
+function getNameFromJSON(obj: string) {
+  return (JSON.parse(obj) as Pizza).name;
+}
 
-// // Object Types
-
-// let pizza: { name: string; price: number; getName(): string } = {
-//   name: 'Plain Old Pepperoni',
-//   price: 20,
-//   getName() {
-//     return pizza.name;
-//   },
-// };
-
-// console.log(pizza.getName());
-
-// // Array Types and Generics
-
-// let sizes: number[];
-
-// sizes = [1, 2, 3];
-
-// let toppings: Array<string>;
-
-// toppings = ['pepperoni', 'tomato', 'bacon'];
-
-// // Tuple Types for Arrays
-
-// let pizza: (string | number | boolean)[];
-
-let pizza: [string, number, boolean]; // strict data type & order
-
-pizza = ['Pepperoni', 20, true];
+getNameFromJSON(serialized);
