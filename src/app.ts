1691b38c6e0b3976aba5750fd75567771d51d37d
Arkadiusz Chrobak <chrobak.arkadiusz@gmail.com>
2018-04-22 14:41:12 +0200

    Classes Properties and Inheritance

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,71 @@
-// // Creating Interfaces // Interfaces with Function Types // Extending Interfaces
-// Interfaces and Optional Properties // Interfaces with Index Signatures
+// // Understanding Classes and Constructors // Public and Private Members // Readonly Members
+// Setters and Getters (Accessors) // Class Inheritance // Abstract Classes // Protected Members and Inheritance
+// Interface contracts with "implements"
 
-interface Sizes {
-  sizes: string[];
-};
-
-// type Pizza = {
-//   name: string;
-//   sizes: string[]
-// };
-
-interface Pizza extends Sizes {
-  name: string;
-  toppings?: number;
+interface SizesInterface {
   // sizes: string[];
-  getAvailableSize(): void;
-  [key: number]: any;
-  // [key: string]: any;
-  dictionary?: {
-    [key: string]: any;
+  availableSizes: string[];
+}
+
+// abstract available only to extends
+abstract class Sizes implements SizesInterface {
+  constructor(protected sizes: string[]) { }
+
+  set availableSizes(sizes: string[]) {
+    this.sizes = sizes;
   }
 
-};
+  get availableSizes() {
+    return this.sizes;
+  }
 
-// type getAvailableSize = () => string[];
+}
 
-let pizza: Pizza;
+interface PizzaInterface extends SizesInterface {
+  readonly name: string;
+  toppings: string[];
+  updateSizes(sizes: string[]): void;
+  addTopping(topping: string): void;
+}
 
-function createPizza(name: string, sizes: string[]): Pizza {
-  return {
-    name,
-    sizes,
-    getAvailableSize() {
-      return this.sizes;
-    }
+class Pizza extends Sizes implements PizzaInterface {
+
+  // public name: string;
+  public toppings: string[] = [];
+
+  constructor(readonly name: string, sizes: string[]) {
+    super(sizes);
+  }
+
+  public updateSizes(sizes: string[]) {
+    this.sizes = sizes;
+  }
+
+  public addTopping(topping: string) {
+    this.toppings.push(topping)
+  }
+
+}
+
+const pizza = new Pizza('Pepperoni', ['small', 'medium']);
+
+pizza.addTopping('pepperoni');
+
+console.log(pizza.availableSizes);
+pizza.updateSizes(['large']);
+
+console.log(pizza.availableSizes);
+// console.log(pizza);
+
+// //  Static Properties and Methods
+
+class Coupon {
+
+  // static Property
+  static allowed = ['Pepperoni', 'Blazing Inferno']
+  static create(percentage: number) {
+    return `PIZZA_RESTAURANTE_${percentage}`
   }
 }
 
-pizza = createPizza('Pepperoni', ['small', 'medium']);
-pizza[1] = 'xyz'
-pizza.toppings = 1;
-
-console.log(pizza)
+console.log(Coupon.create(25))
